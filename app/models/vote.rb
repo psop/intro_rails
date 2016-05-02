bcaafb7556b54927a3a2642d31e89feaad889567
LizHuang <search.psop@gmail.com.tw>
2016-05-02 23:34:45 +0800

    vote新增user_id欄位

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,4 @@
 class Vote < ActiveRecord::Base
 	belongs_to :topic, counter_cache: :votes_count
+	belongs_to :owner, class_name: "User", foreign_key: :user_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502151116) do
+ActiveRecord::Schema.define(version: 20160502152053) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160502151116) do
     t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
