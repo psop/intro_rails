6ee2e0efc7fea04fd5c61c4e0c30a6e5f960715c
LizHuang <search.psop@gmail.com.tw>
2016-07-11 15:57:53 +0800

    adjustment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,11 +1,11 @@
 class Topic < ActiveRecord::Base
-	validates :title, presence: true
-	has_many :votes, dependent: :destroy
-    has_many :upvoted_users, through: :votes, source: :user
-    has_many :downvoted_users, through: :votes, source: :user
-	belongs_to :owner, class_name: "User", foreign_key: :user_id
+  validates :title, presence: true
+  has_many :votes, dependent: :destroy
+  has_many :upvoted_users, through: :votes, source: :user
+  has_many :downvoted_users, through: :votes, source: :user
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
 
-	def editable_by?(user)
-		user && user == owner
-	end
+  def editable_by?(user)
+	user && user == owner
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,11 +1,11 @@
 class Vote < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :topic, counter_cache: :votes_count
-    validates_uniqueness_of :topic_id, scope: :user_id
+  belongs_to :user
+  belongs_to :topic, counter_cache: :votes_count
+  validates_uniqueness_of :topic_id, scope: :user_id
 
-	belongs_to :author, class_name: "User", foreign_key: :user_id
+  belongs_to :author, class_name: "User", foreign_key: :user_id
  
-    def editable_by?(user)
-    	user && user == author
-    end
+  def editable_by?(user)
+    user && user == author
+  end
 end
