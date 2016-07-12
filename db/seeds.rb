1f14fefcf0b6c692e5d49595ecc96a5c01634fbc
LizHuang <search.psop@gmail.com.tw>
2016-07-12 12:56:24 +0800

    撰寫 seed.rb 檔

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Hello World!"
+puts "這個種子檔會自動建立一個帳號, 並且創建 20 個 topics"
+
+create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', name: '測試用帳號'])
+
+create_topics = for i in 1..20 do
+                   Topic.create!([title: "Topic no.#{i}", description: "這是用種子建立的第 #{i} 個討論版", user_id: "1"])
+                end
